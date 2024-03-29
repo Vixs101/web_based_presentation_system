11d8adaf6cc4e9e161b6245fb2af12731d43d5a0
Vixs101 <elijahvix695@gmail.com>
2024-03-29 15:56:18 +0100

    started working on the login and signup page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css';
+import LoginPage from "./pages/LoginPage";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <LoginPage/>,
+  },
+  {
+    path: '/home',
+    element: <Home/>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,27 +1,59 @@
 import React, { useState } from "react";
 
-function LoginPage () {
-  const [loginType, setLoginType] = useState('login');
-  
+function LoginPage() {
+  const [loginType, setLoginType] = useState("login");
+
   return (
-    <div>
-      <section>
-        <h1>Welcome to Craft School</h1>
+    <div className="flex items-center justify-center h-full w-full bg-blue-300">
+      <section className="flex flex-col gap-3 height w-11/12 md:w-1/2 p-5 bg-white rounded-2xl text-center items-center">
+        <h1 className="text-2xl md:text-4xl font-bold text-gray-800">
+          Welcome to Craft School
+        </h1>
         <p>Login or create an account to continue</p>
-        <div>
+        <div className="flex gap-3">
           <button
-            className={`btn ${loginType == "login" ? "selected" : ""}`}
+            className={`rounded-lg text-white font-bold p-2 bg-blue-300 ${
+              loginType == "login" ? "bg-slate-400" : ""
+            }`}
             onClick={() => setLoginType("login")}
           >
             Login
           </button>
           <button
-            className={`btn ${loginType == "signup" ? "selected" : ""}`}
+            className={`rounded-lg text-white font-bold p-2 bg-blue-300 ${
+              loginType == "signup" ? "bg-slate-400" : ""
+            }`}
             onClick={() => setLoginType("signup")}
           >
             Signup
           </button>
         </div>
+        <form className="w-3/4">
+          <div className="flex flex-col items-start">
+            <label htmlFor="email">Email *</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className=""
+            />
+          </div>
+
+          <div className="flex flex-col items-start">
+            <label htmlFor="password">Password *</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          {loginType == "login" ? (
+            <button>Login</button>
+          ) : (
+            <button>Sign up</button>
+          )}
+        </form>
       </section>
     </div>
   );
