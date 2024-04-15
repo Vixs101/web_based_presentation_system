1fab332a62f916ca0f8f26f1c3e1ebbd89c240eb
Vixs101 <elijahvix695@gmail.com>
2024-04-15 17:39:20 +0100

    finished the video upload feature with meta data

--- src/pages/UploadVideos.tsx
+++ src/pages/UploadVideos.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   getStorage,
   ref,
@@ -7,12 +7,14 @@ import {
   listAll,
 } from "firebase/storage";
 import { storage } from "../firebase/config";
-import {v4} from 'uuid';
+import { v4 } from "uuid";
 
 function UploadVideos() {
   const [videoUpload, setVideoUpload] = useState<File | null>(null);
-  const [videoList, setVideoList] = useState([]);
-  
+  const [videoList, setVideoList] = useState<string[]>([]);
+  const [videoTitle, setVideoTitle] = useState("");
+  const [channelName, setChannelName] = useState("");
+
   const videoListRef = ref(storage, "videos/");
 
   // handle the file selection procedure
@@ -25,30 +27,45 @@ function UploadVideos() {
     }
   };
 
-  
   // handle the uploading procedure
   const handleUpload = () => {
     if (videoUpload == null) return;
 
     const videoRef = ref(storage, `videos/${videoUpload.name + v4()}`);
 
-    uploadBytes(videoRef, videoUpload).then(() => {
-      alert('image uploaded')
-    })
+    const metadata = {
+      customMetadata: {
+        title: videoTitle,
+        channel: channelName,
+      },
+    };
+
+    try {
+      uploadBytes(videoRef, videoUpload, metadata).then((snapshot) => {
+        alert("video uploaded");
+        setVideoUpload(null);
+        setVideoTitle("");
+        setChannelName("");
+
+        getDownloadURL(snapshot.ref).then((url) => {
+          setVideoList((prev) => [...prev, url]);
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   //
   useEffect(() => {
     listAll(videoListRef).then((response) => {
       response.items.forEach((item) => {
-        getDownloadURL(item.then((url) => {
+        getDownloadURL(item).then((url) => {
           setVideoList((prev) => [...prev, url]);
-
-        }))
-      })
-    })
-  }, [])
-  
+        });
+      });
+    });
+  }, []);
 
   return (
     <>
@@ -64,10 +81,31 @@ function UploadVideos() {
             <path d="M15 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 17.25 7.5h-1.875A.375.375 0 0 1 15 7.125V5.25ZM4.875 6H6v10.125A3.375 3.375 0 0 0 9.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V7.875C3 6.839 3.84 6 4.875 6Z" />
           </svg>
           <h2>Choose a video file</h2>
+          {/* input file */}
           <input
             type="file"
             onChange={handleFileChange}
             className="rounded-xl text-gray-800 font-bold p-2 mt-3 self-center text-center cursor-pointer bg-[#deb887] w-3/4"
+          />
+          {/* input video title */}
+          <input
+            type="text"
+            placeholder="Enter Video Title"
+            value={videoTitle}
+            onChange={(e) => {
+              setVideoTitle(e.target.value);
+            }}
+            className="rounded-xl text-gray-800 p-2 mt-3 self-center text-center cursor-pointer  w-3/4 outline-none border-2 border-[#deb887]"
+          />
+          {/* input channel name */}
+          <input
+            type="text"
+            placeholder="Enter Channel Name"
+            value={channelName}
+            onChange={(e) => {
+              setChannelName(e.target.value);
+            }}
+            className="rounded-xl text-gray-800 p-2 mt-3 self-center text-center cursor-pointer  w-3/4 outline-none border-2 border-[#deb887]"
           />
           <button
             className="rounded-xl text-gray-800 font-bold p-2 bg-[#deb887] mx-auto hover:bg-[#efcfa4] mt-3 self-center w-1/2 md:w-1/4"
@@ -76,6 +114,24 @@ function UploadVideos() {
           >
             Upload Video
           </button>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-7 mt-5 md:gap-10 max-w-full ">
+          {videoList.map((url, index) => {
+            return (
+              <div className="flex flex-col gap-3 border bg-[#deb887] rounded-xl text-gray-800">
+                <iframe
+                  key={index}
+                  src={url}
+                  allowFullScreen
+                  className="w-full rounded-t-xl"
+                ></iframe>
+                <div className="flex flex-col gap-3 ml-3 mb-3 ">
+                  <h2 className="text-base font-extrabold">Title: {}</h2>
+                  <p className="">Channel: {}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
